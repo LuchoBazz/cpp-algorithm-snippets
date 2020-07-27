a2f7e2b2ad6ade5ed5270a993588fae96ba52dd0
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-07-27 16:43:33 -0500

    Macros and Geometry Template was added

---
+++ macros/main_macros.cpp
@@ -0,0 +1,9 @@
+#define endl '\n'
+#define precise(n,k) fixed << setprecision(k) << n
+#define all(x) (x).begin(), (x).end()
+#define rall(x) (x).rbegin(), (x).rend()
+
+#define uint  unsigned int
+#define ll    long long
+#define ull   unsigned long long
+#define ld    long double
