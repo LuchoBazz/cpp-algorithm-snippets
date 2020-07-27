5b7ed6f2d7451b09d27c1c537395dc0946274622
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-07-27 11:39:36 -0500

    C++ Template Debugging was added

---
+++ debugging/other_to_string.cpp
@@ -0,0 +1,23 @@
+template <typename A, typename B>
+string to_string(const pair<A, B> p) {
+    return "(" + to_string(p.first) + ", " + to_string(p.second) + ")";
+}
+
+template <typename A, typename B, typename C>
+string to_string(const tuple<A, B, C> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ")";
+}
+ 
+template <typename A, typename B, typename C, typename D>
+string to_string(const tuple<A, B, C, D> p) {
+    return "(" + to_string(get<0>(p)) + ", " + to_string(get<1>(p)) + ", " + to_string(get<2>(p)) + ", " + to_string(get<3>(p)) + ")";
+}
+
+template <size_t N>
+string to_string(const bitset<N> v) {
+    string res = "";
+    for (size_t i = 0; i < N; i++) {
+        res += static_cast<char>('0' + v[i]);
+    }
+    return res;
+}
