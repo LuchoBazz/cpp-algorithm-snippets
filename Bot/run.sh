9b92702e43f127302c0fcbd570cb25ab101188ea
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-08-05 21:15:27 -0500

    Bot was removed

--- Bot/run.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-# @author  : Luis Miguel Baez <es.luismiguelbaez@gmail.com>
-
-# Flags
-
-./script.sh "$1" "$2" "$3"
-
-./script_diff.sh "$1"
--- Bot/testing.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-for i in $(find -name '*.cpp'); do
-   [ -f "$i" ] || break
-   echo "$i --> ${i##*/}"
-done
