fbf9ef531b506eb3bd977450b3c478d86ae5f3b2
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-08-06 15:14:12 -0500

    Math algorithms and Segment Tree template were added

---
+++ math/math_prime_factors.cpp
@@ -0,0 +1,24 @@
+// usage:
+//   int n = 100;
+//   vector<int> factors = primeFactors<int>(n);
+//   {2, 2, 5, 5}
+//   2*2*5*5 = 2^2 * 5^2 = 100
+
+template<class T>
+vector<T> primeFactors(T number)  {
+    vector<T> factors;
+    while (number % 2 == 0) {
+        factors.push_back(2);
+        number = number / 2;
+    }
+    for (T i = 3; i*i <= number; i += 2) {
+        while (number % i == 0) {
+            factors.push_back(i);
+            number = number / i; 
+        }
+    }
+    if (number > 2)  {
+        factors.push_back(number);
+    }
+    return factors;
+}
