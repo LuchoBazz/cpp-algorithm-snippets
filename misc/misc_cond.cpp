5913ef4e5494058440b29dbe6b69bab255c09b7f
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-09-16 18:07:38 -0500

    Macros and Debugging was rename to Misc and (misc_vector_n_d and misc_cond) were added

---
+++ misc/misc_cond.cpp
@@ -0,0 +1,3 @@
+template<typename T>
+bool between(T a, T b, T c) { return (a <= b && b <= c); }
+// #define between(a, b, c) (a <= b && b <= c)
---
+++ misc/misc_vector_n_d.cpp
@@ -0,0 +1,12 @@
+template<typename T>
+vector<vector<T>> vector2d(int x1, int x2, T default_value=T()) {
+    return vector<vector<T>>(x1, vector<T>(x2, default_value));
+}
+template<typename T>
+vector<vector<vector<T>>> vector3d(int x1, int x2, int x3, T default_value=T()) {
+    return vector<vector<vector<T>>>(x1, vector<vector<T>>(x2, vector<T>(x3, default_value)));
+}
+template<typename T>
+vector<vector<vector<vector<T>>>> vector4d(int x1, int x2, int x3, int x4, T default_value=T()) {
+    return vector<vector<vector<vector<T>>>>(x1, vector<vector<vector<T>>>(x2, vector<vector<T>>(x3, vector<T>(x3, default_value))));
+}
