16373957c4637e174b2b0b0df8f3c4ed6a61e578
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-10-09 17:05:43 -0500

    Polynomial class was updated

--- math/math_polynomial.cpp
+++ math/math_polynomial.cpp
@@ -15,12 +15,13 @@ public:
     Polynomial(const vector<T> &p) : poly(p) {check_data_types();}
 
     int degree() {
+        // degree of the polynomial
         return int(poly.size()) - 1;
     }
 
     Polynomial<T> scale(int n) {
-        // Multiply polynomial ``p(x)`` with ``x^n``.
-        // If n is negative, poly ``p(x)`` is divided with ``x^n``, and remainder is
+        // Multiply polynomial p(x) with x^n.
+        // If n is negative, poly p(x) is divided with x^n, and remainder is
         // discarded (truncated division).
         Polynomial<T> ans;
         ans.poly.reserve(2*n);
@@ -36,8 +37,8 @@ public:
     }
 
     Polynomial<T> extend(int d) {
-        // Extend list ``p`` representing a polynomial ``p(x)`` to
-        // match polynomials of degree ``d-1``.
+        // Extend list p representing a polynomial p(x) to
+        // match polynomials of degree d-1.
         int sz = d-int(poly.size());
         if(sz <= 0) return *this;
         Polynomial<T> ans(sz, T(0));
@@ -49,7 +50,7 @@ public:
     }
 
     Polynomial<T> norm() {
-        // Normalize the polynomial ``p(x)`` to have a non-zero most significant
+        // Normalize the polynomial p(x) to have a non-zero most significant
         // coefficient.
         // [0, 0, 3, 1] -> [3, 1]
         vector<T> ans;
@@ -65,71 +66,66 @@ public:
         return Polynomial<T>(ans);
     }
 
-    // Scalar Multiplication
     Polynomial<T> operator*(T number) {
-        // Multiply polynomial ``p(x)`` with scalar (constant) ``a``.
+        // Multiply polynomial p(x) with scalar (constant) a.
         for(int i = 0; i < int(poly.size()); ++i){
             poly[i] *= number;
         }
         return *this;
     }
 
-    // Add Polynomial
     Polynomial<T> operator+(const Polynomial<T> &other) {
-        // Add polynomials ``u(x)`` and ``v(x)``.
+        // Add polynomials f(x) and g(x).
         int n = int(poly.size());
         int m = int(other.poly.size());
         int sz = max(n, m);
-        Polynomial<T> A(poly);
-        Polynomial<T> B(other.poly);
+        Polynomial<T> fx(poly);
+        Polynomial<T> gx(other.poly);
         Polynomial<T> ans(sz, 0);
-        A.extend(sz);
-        B.extend(sz);
+        fx.extend(sz);
+        gx.extend(sz);
         for(int i = 0; i < sz; ++i){
-            ans.poly[i] = A.poly[i] + B.poly[i];
+            ans.poly[i] = fx.poly[i] + gx.poly[i];
         }
         return ans;
     }
 
-    // Sub Polynomial
     Polynomial<T> operator-(const Polynomial<T> &other) {
-        // Subtract polynomials ``u(x)`` and ``v(x)``."""
+        // Subtract polynomials f(x) and g(x).
         int n = int(poly.size());
         int m = int(other.poly.size());
         int sz = max(n, m);
-        Polynomial<T> A(poly);
-        Polynomial<T> B(other.poly);
+        Polynomial<T> fx(poly);
+        Polynomial<T> gx(other.poly);
         Polynomial<T> ans(sz, 0);
-        A.extend(sz);
-        B.extend(sz);
+        fx.extend(sz);
+        gx.extend(sz);
         for(int i = 0; i < sz; ++i){
-            ans.poly[i] = A.poly[i] - B.poly[i];
+            ans.poly[i] = fx.poly[i] - gx.poly[i];
         }
         return ans;
     }
 
-    // Multiply Polynomial
     Polynomial<T> operator*(Polynomial<T> &other) {
-        // Multiply polynomials ``u(x)`` and ``v(x)`` with FFT.
-        vector<complex<double>> A(poly.rbegin(), poly.rend());
-        vector<complex<double>> B(other.poly.rbegin(), other.poly.rend());
+        // Multiply polynomials f(x) and g(x) with FFT.
+        vector<complex<double>> fx(poly.rbegin(), poly.rend());
+        vector<complex<double>> gx(other.poly.rbegin(), other.poly.rend());
         int n = 1;
         while (n < int(poly.size()) + int(other.poly.size())) n <<= 1;
-        A.resize(n);
-        B.resize(n);
+        fx.resize(n);
+        gx.resize(n);
 
-        fft(A);
-        fft(B);
-        for (int i = 0; i < n; i++) A[i] *= B[i];
-        fft(A, true);
+        fft(fx);
+        fft(gx);
+        for (int i = 0; i < n; i++) fx[i] *= gx[i];
+        fft(fx, true);
 
         vector<T> result(n);
-        for (int i = 0; i < n; i++) result[i] = round(A[i].real());
+        for (int i = 0; i < n; i++) result[i] = round(fx[i].real());
         reverse(result.begin(), result.end());
         Polynomial<T> ans(result);
         return ans.norm();
     }
-
 };
 
 using Poly = Polynomial<int>;
