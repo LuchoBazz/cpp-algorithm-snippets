f77829e4ec4db045d5c81238dd4d940642c48802
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-10-29 21:12:10 -0500

    Bellman ford algorithm was updated

--- graph/graph_bellman_ford.cpp
+++ graph/graph_bellman_ford.cpp
@@ -8,17 +8,12 @@ vector<T> bellman_ford(const undigraph<T> &G, int source, bool &cycle) {
     T inf = T(numeric_limits<T>::max() / 2);
     vector<T> dist(G.n, inf);
     dist[source] = T(0);
-    for(int i = 0; i < G.n; ++i){
+    for(int i = 0; i < G.n + 1; ++i){
         for(const edge<T> &e: G.edges) {
             if(dist[e.from] + e.cost < dist[e.to] && dist[e.from] != inf) {
                 dist[e.to] = dist[e.from] + e.cost;
+                if(i == n) cycle = true; // There are negative edges
             }
-        }
-    }
-    for(const edge<T> &e: G.edges) {
-        if(dist[e.from] + e.cost < dist[e.to] && dist[e.from] != inf) {
-            cycle = true;
-            // There are negative edges
         }
     }
     return dist;
