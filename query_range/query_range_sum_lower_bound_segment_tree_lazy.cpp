a53438bc1b440d9a4e83b81b3355a13e3d209ad8
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-11-17 10:41:18 -0500

    segment tree lazy snippets were added

---
+++ query_range/query_range_sum_lower_bound_segment_tree_lazy.cpp
@@ -0,0 +1,10 @@
+// Returns min(p | p <=right && sum[left..p]>=sum). If no such p exists, returns -1
+template<typename T>
+int sum_lower_bound(SegmentTree<T> &st, int left, int right, T sum) {
+    T accum_sum = T(0);
+    return st.find_first(left, right, [&](const typename SegmentTree<T>::Node &node) {
+        if (accum_sum + node.sum >= sum) return true;
+        accum_sum += node.sum;
+        return false;
+    });
+}
