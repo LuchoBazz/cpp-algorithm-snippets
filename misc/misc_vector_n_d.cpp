041e02e9d7c3215edb4077c528b8e37777b5f1e1
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2020-11-19 22:32:36 -0500

    Misc vector nd was updated

--- misc/misc_vector_n_d.cpp
+++ misc/misc_vector_n_d.cpp
@@ -6,13 +6,13 @@ vt<vt<T>> vector2d(int x1, int x2, T default_value=T()) {
 }
 template<typename T>
 vt<vt<vt<T>>> vector3d(int x1, int x2, int x3, T default_value=T()) {
-    return vt<vt<vt<T>>>(x1, vt<vt<T>>(x2, vt<T>(x3, default_value)));
+    return vt<vt<vt<T>>>(x1, vector2d<T>(x2, x3, default_value));
 }
 template<typename T>
 vt<vt<vt<vt<T>>>> vector4d(int x1, int x2, int x3, int x4, T default_value=T()) {
-    return vt<vt<vt<vt<T>>>>(x1, vt<vt<vt<T>>>(x2, vt<vt<T>>(x3, vt<T>(x4, default_value))));
+    return vt<vt<vt<vt<T>>>>(x1, vector3d<T>(x2, x3, x4, default_value));
 }
 template<typename T>
 vt<vt<vt<vt<vt<T>>>>> vector5d(int x1, int x2, int x3, int x4, int x5, T default_value=T()) {
-    return vt<vt<vt<vt<vt<T>>>>>(x1, vt<vt<vt<vt<T>>>>(x2, vt<vt<vt<T>>>(x3, vt<vt<T>>(x4, vt<T>(x5, default_value)))));
+    return vt<vt<vt<vt<vt<T>>>>>(x1, vector4d<T>(x2, x3, x4, x5, default_value));
 }
