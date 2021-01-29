97cc7a0e2139b9da5363358148f81c26f2bf0bd7
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2021-01-29 18:29:21 -0500

    middle for was changed

--- searches/searches_binary_search_II.cpp
+++ searches/searches_binary_search_II.cpp
@@ -1,7 +1,7 @@
 int n = oo;
 int low = 0, high = n, mid;
 while (high - low > 1) {
-    mid = low + (high - low) >> 1;
+    mid = low + (high - low) / 2;
     if(!ok(mid)) {
         low = mid;
     } else {
