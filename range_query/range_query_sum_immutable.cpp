3be6f2625e41f3e8e2b57a1f65b2824646ab5139
Luis Miguel Baez Aponte <es.luismiguelbaez@gmail.com>
2021-02-05 15:29:50 -0500

    range query sum 2d immutable was added

--- range_query/range_query_sum_immutable.cpp
+++ range_query/range_query_sum_immutable.cpp
@@ -5,6 +5,7 @@ public:
     int n;
     vector<T> dp;
  
+    PrefixSum() n(-1) {}
     PrefixSum(vector<T>& nums) {
         n = (int) nums.size();
         if(n == 0)
@@ -15,7 +16,7 @@ public:
             dp[i] = dp[i-1] + nums[i-1];
     }
     
-    T sum(int left, int right) {
+    T query(int left, int right) {
         assert(0 <= left && left <= right && right <= n - 1);
         return dp[right+1] - dp[left];
     }
