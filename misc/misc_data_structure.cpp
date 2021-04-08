25fc10596f67c02cbcd3fc33d524736954823e3a
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-04-08 18:44:32 -0500

    find_nearest function was added

--- misc/misc_data_structure.cpp
+++ misc/misc_data_structure.cpp
@@ -34,6 +34,25 @@ T erase_max(set<T> &st) {
     return to_return;
 }
 
+// Finds the element nearest to target
+template<typename T>
+T find_nearest(set<T> &st, T target) {
+    assert(!st.empty());
+    auto it = st.lower_bound(target);
+    if (it == st.begin()) {
+        return *it;
+    } else if (it == st.end()) {
+        it--; return *it;
+    }
+    T left = *it; it--;
+    T right = *it;
+    if (target-right < left-target) 
+        return right;
+    // if they are the same distance, choose right
+    // if you want to choose left change to <=
+    return left;
+}
+
 template<typename T>
 class to_reverse {
 private:
