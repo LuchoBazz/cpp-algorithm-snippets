117df785803b11259f6d74c95803af34f42d8e99
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-04-08 14:43:34 -0500

    misc data structure were updated

--- misc/misc_data_structure.cpp
+++ misc/misc_data_structure.cpp
@@ -6,3 +6,40 @@ using max_heap = template_heap<T>;
 
 template<typename T>
 using min_heap = template_heap<T, vector<T>, greater<T>>;
+
+template<typename T>
+T get_min(set<T> &st) {
+    assert(!st.empty());
+    return *st.begin();
+}
+template<typename T>
+T get_max(set<T> &st) {
+    assert(!st.empty());
+    return *st.rbegin();
+}
+
+template<typename T>
+T erase_min(set<T> &st) {
+    assert(!st.empty());
+    T to_return = get_min(st);
+    st.erase(st.begin());
+    return to_return;
+}
+
+template<typename T>
+T erase_max(set<T> &st) {
+    assert(!st.empty());
+    T to_return = get_max(st);
+    st.erase(--st.end());
+    return to_return;
+}
+
+template<typename T>
+class to_reverse {
+private:
+    T& iterable_;
+public:
+    explicit to_reverse(T& iterable) : iterable_{iterable} {}
+    auto begin() const { return rbegin(iterable_); }
+    auto end() const { return rend(iterable_); }
+};
