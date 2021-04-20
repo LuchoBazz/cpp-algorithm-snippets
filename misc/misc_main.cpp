800c303a2fe72fed13956658c5a066c9c0abad3e
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-04-19 21:10:28 -0500

    loops in misc main were updated and distance_divisible was added

--- misc/misc_main.cpp
+++ misc/misc_main.cpp
@@ -1,8 +1,8 @@
 #define endl '\n'
-#define finally(value) {cout << (value) << '\n'; return;}
-#define forn(i, b) for(int i = 0; i < (int) b; ++i)
-#define rep(i, a, b) for(int i = int(a); i < (int) b; ++i)
-#define trav(x, a) for(auto &x: a)
+#define forn(i, b) for(int i = 0; i < int(b); ++i)
+#define rep(i, a, b) for(int i = int(a); i < int(b); ++i)
+#define rev(i, a, b) for(int i = int(b); i >= int(a); i--)
+#define trav(ref, ds) for(auto &ref: ds)
 #define sz(v) ((int) v.size())
 
 using int64 = int64_t;
--- misc/misc_math.cpp
+++ misc/misc_math.cpp
@@ -3,12 +3,19 @@
 #define eps  1e-9
 
 template<typename T>
+int cmp(const T &a, const T &b) {
+    return ( (a + eps < b)? -1 :( (b + eps < a )? 1 : 0) );
+}
+
+template<typename T>
 T ceiling_division(T numerator, T denominator) {
     assert(denominator != static_cast<T>(0));
     return (numerator+denominator-1)/denominator;
 }
 
+// How much does it need to add to n so that it is divisible by k
 template<typename T>
-int cmp(const T &a, const T &b) {
-    return ( (a + eps < b)? -1 :( (b + eps < a )? 1 : 0) );
+T distance_divisible(T n, T k) {
+    assert(0 < k); if(n < k) return k - n % k;
+    return n % k;
 }
