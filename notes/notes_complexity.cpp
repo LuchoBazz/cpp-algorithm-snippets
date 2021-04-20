8ff537164bf8670c6692113d2a264099d062dbe3
sorkierkegaard <es.luismiguelbaez@gmail.com>
2021-04-19 20:43:57 -0500

    Notes and comments were added

---
+++ notes/notes_complexity.cpp
@@ -0,0 +1,12 @@
+// Estimating efficiency
+
+// Input size  ---------- Required time complexity
+// n <= 10                O(n!)
+// n <= 20                O(2^n)
+// n <= 500               O(n^3)
+// n <= 5000              O(n^2)
+// n <= 2e5               O(n*sqrt(n))
+// n <= 1e6               O(n*log2(n))
+// n <= 1e8               O(n)
+// n <= 1e16              O(sqrt(n))
+// n is large             O(1) or O(logn)
---
+++ notes/notes_time_and_memory_limits.cpp
@@ -0,0 +1,12 @@
+// THINGS TO KEEP IN MIND
+//   * int overflow, time and memory limits
+//   * Special case (n = 1?)
+//   * Do something instead of nothing and stay organized
+//   * Don't get stuck in one approach
+
+// TIME AND MEMORY LIMITS
+//   * 1 second is approximately 10^8 operations (c++)
+//   * 10^6 Elements of 32 Bit (4 bytes) is equal to 4 MB
+//   * 62x10^6 Elements of 32 Bit (4 bytes) is equal to 250 MB
+//   * 10^6 Elements of 64 Bits (8 bytes) is equal to 8 MB
+//   * 31x10^6 Elements of 64 Bit (8 bytes) is equal to 250 MB
