28796f0b701c7f3fe505f799a6d401646eeef04b
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-06-20 09:49:08 -0500

    feat: leetcode template was updated

--- template/template_std_leetcode.cpp
+++ template/template_std_leetcode.cpp
@@ -8,6 +8,7 @@ using namespace std;
 #define forn(i, b) for(int i = 0; i < int(b); ++i)
 #define rep(i, a, b) for(int i = int(a); i <= int(b); ++i)
 #define trav(ref, ds) for(auto &ref: ds)
-#define sz(v) ((int) v.size())
+#define SZ(v) ((int) v.size())
+#define _ << ' ' <<
 
 using int64 = int64_t;
