95af28423f155d8cc82a05565c941c57ecdca352
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-07-29 17:06:29 -0500

    feat: fast_mod was added

--- numeric/numeric_mod.cpp
+++ numeric/numeric_mod.cpp
@@ -1,22 +1,10 @@
-const int MOD = int(1e9+7);
+const int md = 1e9 + 7;
 
-template<typename T>
-T sub(T a, T b) {
-    return (1LL*(a-b)%MOD + MOD) % MOD;
-}
+template<typename T> T sub(T a, T b) { return (1LL*(a-b)%md + md) % md; }
+template<typename T> T add(T a, T b) { return (1LL*(a%md) + 1LL*(b%md)) % md; }
+template<typename T> T mul(T a, T b) { return (1LL*(a%md) * (b%md)) % md; }
 
-template<typename T>
-T add(T a, T b) {
-    return (1LL*(a%MOD) + 1LL*(b%MOD)) % MOD;
-}
-
-template<typename T>
-T mul(T a, T b) {
-    return (1LL*(a%MOD) * (b%MOD)) % MOD;
-}
-
-template<typename T, typename U>
-T fastpow(T a, U b) {
+template<typename T, typename U> T fastpow(T a, U b) {
     assert(0 <= b);
     T answer = static_cast<T>(1);
     while (b > 0) {
@@ -29,22 +17,18 @@ T fastpow(T a, U b) {
     return answer;
 }
 
-template<typename T>
-T inverse(T a) {
-    a %= MOD;
-    if (a < 0) a += MOD;
-    T b = MOD, u = 0, v = 1;
+template<typename T> T inverse(T a) {
+    a %= md;
+    if (a < 0) a += md;
+    T b = md, u = 0, v = 1;
     while (a) {
         T t = b / a;
         b -= t * a; swap(a, b);
         u -= t * v; swap(u, v);
     }
     assert(b == 1);
-    if (u < 0) u += MOD;
+    if (u < 0) u += md;
     return u;
 }
 
-template<typename T>
-T division(T a, T b) {
-    return mul(a, inverse(b));
-}
+template<typename T> T division(T a, T b) { return mul(a, inverse(b)); }
