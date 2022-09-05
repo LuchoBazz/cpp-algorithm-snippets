191e90802de05294c91fae6d16190a82703fb63e
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2022-09-05 10:06:01 -0500

    feat: modular libary was updated

--- numeric/numeric_mod.cpp
+++ numeric/numeric_mod.cpp
@@ -1,34 +1,40 @@
+
 const int md = 1e9 + 7;
 
-template<typename T> T sub(T a, T b) { return (1LL*(a-b)%md + md) % md; }
-template<typename T> T add(T a, T b) { return (1LL*(a%md) + 1LL*(b%md)) % md; }
-template<typename T> T mul(T a, T b) { return (1LL*(a%md) * (b%md)) % md; }
+template<typename T> inline T add(T a, T b, const int& mod) { return (a+b>=mod)? a+b-mod : a+b; }
+template<typename T> inline T sub(T a, T b, const int& mod) { return (a-b<0)? a-b+mod : a-b; }
+template<typename T> inline T mul(T a, T b, const int& mod) { return (1LL*a*b)%mod; }
 
-template<typename T, typename U> T fastpow(T a, U b) {
+template<typename T, typename U> T fastpow(T a, U b, const int& mod) {
     assert(0 <= b);
     T answer = static_cast<T>(1);
     while (b > 0) {
-        if (b & 1) {
-            answer = mul(answer, a);
-        }
-        a = mul(a, a);
+        if (b & 1) answer = mul(answer, a, mod);
+        a = mul(a, a, mod);
         b >>= 1;
     }
     return answer;
 }
 
-template<typename T> T inverse(T a) {
-    a %= md;
-    if (a < 0) a += md;
-    T b = md, u = 0, v = 1;
+template<typename T> T inverse(T a, const int& mod) {
+    a %= mod;
+    if (a < 0) a += mod;
+    T b = mod, u = 0, v = 1;
     while (a) {
         T t = b / a;
         b -= t * a; swap(a, b);
         u -= t * v; swap(u, v);
     }
     assert(b == 1);
-    if (u < 0) u += md;
+    if (u < 0) u += mod;
     return u;
 }
 
-template<typename T> T division(T a, T b) { return mul(a, inverse(b)); }
+template<typename T> T division(T a, T b, const int& mod) { return mul(a, inverse(b, mod), mod); }
+
+// Notes:
+// 1) a^(b^(a)) % md
+//    is equal to:
+//    c++ code:
+//    int exponent = fastpow(b, c, md-1);
+//    int answer = fastpow(a, exponent, md);
