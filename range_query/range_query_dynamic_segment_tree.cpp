84dcff1ead3373e10cf3529ac6bbb9c79ae71707
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2023-10-03 15:40:04 -0500

    feat: update Dynamic Segment Tree

--- range_query/range_query_dynamic_segment_tree.cpp
+++ range_query/range_query_dynamic_segment_tree.cpp
@@ -1,5 +1,4 @@
-template<typename T>
-struct Node {
+template <typename T> struct Node {
     // set default values
     T sum = ...;
     T mx = ...;
@@ -16,63 +15,66 @@ struct Node {
     }
 };
 
-template<typename T>
-class SegmentTree {
+template <typename T> class SegmentTree {
     SegmentTree<T> *left, *right;
     int low, high, mid;
     Node<T> data;
- 
-    SegmentTree(int low, int high) 
-            : low(low), high(high), data{} {
+
+    SegmentTree(int low, int high) : low(low), high(high), data{} {
         mid = (high + low) / 2;
-        if(low != high) {
+        if (low != high) {
             left = new SegmentTree<T>(low, mid);
-            right = new SegmentTree<T>(mid+1, high);
+            right = new SegmentTree<T>(mid + 1, high);
         }
     }
+
 public:
     SegmentTree(const vector<T> &v) {
-        low = 0; high = int(v.size()) - 1;
+        low = 0;
+        high = int(v.size()) - 1;
         mid = (high + low) / 2;
-        if(low != high) {
+        if (low != high) {
             left = new SegmentTree<T>(low, mid);
-            right = new SegmentTree<T>(mid+1, high);
+            right = new SegmentTree<T>(mid + 1, high);
         }
-        for(int i = 0; i < (int) v.size(); ++i) {
-            this->modify(i, v[i]);
+        int idx = 0;
+        for (const T &x : v) {
+            this->modify(idx++, x);
         }
     }
- 
-    Node<T> unite(const Node<T> &a, const Node<T> &b) {
+
+    inline Node<T> unite(const Node<T> &a, const Node<T> &b) {
         Node<T> res;
         res.sum = a.sum + b.sum;
         res.mx = max(a.mx, b.mx);
         res.mn = min(a.mn, b.mn);
-        res.idx_mx = (res.mx==a.mx)?a.idx_mx:b.idx_mx;
-        res.idx_mn = (res.mn==a.mn)?a.idx_mn:b.idx_mn;
+        res.idx_mx = (res.mx == a.mx) ? a.idx_mx : b.idx_mx;
+        res.idx_mn = (res.mn == a.mn) ? a.idx_mn : b.idx_mn;
         return res;
     }
- 
-    void modify(int index, T value) {
-        if(low == high) {
+
+    inline void modify(int index, T value) {
+        if (low == high) {
             data.apply(low, value);
         } else {
-            if(index <= mid) left->modify(index, value);
-            else right->modify(index, value);
+            if (index <= mid)
+                left->modify(index, value);
+            else
+                right->modify(index, value);
             data = unite(left->data, right->data);
         }
     }
- 
-    Node<T> query(int a, int b) {
-        if(a > high || b < low) return Node<T>{};
-        if(a <= low && high <= b) return data;
+
+    inline Node<T> query(int a, int b) {
+        if (a > high || b < low)
+        return Node<T>{};
+        if (a <= low && high <= b)
+        return data;
         return unite(left->query(a, b), right->query(a, b));
     }
 };
 
-template<typename T>
-using segtree = SegmentTree<T>;
-
+template <typename T> using segtree = SegmentTree<T>;
 // Usage:
 // segtree<int64> st(v);
 // st.query(l, r).sum;
