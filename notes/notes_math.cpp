11cebb41c244b8dc4c9843246ee06c34f7b38db0
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-05-14 17:08:52 -0500

    docs(math): Add divisibile by 4 properties

--- notes/notes_math.cpp
+++ notes/notes_math.cpp
@@ -1,1 +1,6 @@
 // A number is divisible by 9 when the sum of its digits is 9 or a multiple of 9.
+
+// A number is divisible by 4 when the last 2 digits are divisible by 4.
+//    - 12: The last two digits are "12", which is divisible by 4, therefore 12 is divisible by 4.
+//    - 156: The last two digits are "56", which is divisible by 4, therefore, 156 is divisible by 4.
+//    - Note: This trick works because 100 is a multiple of 4, so any number ending in two zeros is divisible by 4.
