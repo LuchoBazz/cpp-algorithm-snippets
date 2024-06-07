b29c2fb2b774eebe2665d39474f88564114e85f7
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-07 10:46:07 -0500

    chore(formatter): update template_usaco

--- template/template_usaco.cpp
+++ template/template_usaco.cpp
@@ -6,77 +6,49 @@
 
 using namespace std;
 
-#define endl '\n'
-#define _ << ' ' <<
-#define PB push_back
-#define SZ(v) ((int) v.size())
-#define all(v) (v).begin(), (v).end()
-#define rall(v) (v).rbegin(), (v).rend()
-#define overload4(a, b, c, d, e, ...) e
-#define for1(a) for(int i = 0; i < int(a); ++i)
-#define for2(i, a) for(int i = 0; i < int(a); ++i)
-#define for3(i, a, b) for(int i = int(a); i <= int(b); ++i)
-#define for4(i, a, b, c) for(int i = int(a); i <= int(b); i += int(c))
-#define forn(...) overload4(__VA_ARGS__, for4, for3, for2, for1)(__VA_ARGS__)
- 
-template <class T, class S>
-inline bool xmax(T &a, const S &b) {
-    return (a < b ? a = b, 1 : 0);
-}
-template <class T, class S>
-inline bool xmin(T &a, const S &b) {
-    return (a > b ? a = b, 1 : 0);
-}
- 
-using int64 = long long;
- 
-#ifdef DEBUG 
-#include "debug.h"
-#else
-#define debug(...) 42
-#endif
-
 class Task {
-private:
-
-    void clear() {}
-    void read() {}
+  private:
+    void clear() { ; }
+    void read() { ; }
 
     void solveOne() {
         ;
     }
 
-public:
+  public:
     void solve() {
-        int T=1;
+        int T = 1;
         cin >> T;
-        while (T --> 0) {
+        while (T-- > 0) {
             clear();
             read();
             solveOne();
         }
     }
-private:
-    void print() {cout << "\n";}
-    template<class H, class... T> void print(const H& h, const T&... t) {
+
+  private:
+    void print() { cout << '\n'; }
+    template <class H, class... T> void print(const H &h, const T &...t) {
         cout << h;
-        if(sizeof...(t)) cout << ' '; print(t...);
+        if (sizeof...(t))
+            cout << ' ';
+        print(t...);
     }
 };
 
-void set_usaco(string filename="") {
-    if((int) filename.size() > 0) {
-        freopen((filename+".in").c_str(), "r", stdin);
-        freopen((filename+".out").c_str(), "w", stdout);
+void set_usaco(string filename = "") {
+    if ((int)filename.size() > 0) {
+        freopen((filename + ".in").c_str(), "r", stdin);
+        freopen((filename + ".out").c_str(), "w", stdout);
     }
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
 
     set_usaco("");
-    
+
     Task solver;
     solver.solve();
     cout.flush();
