d59c5a10941e85cb07edb5f75964ecb01a9ab7ac
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-11 10:27:46 -0500

    chore(formatting): apply code formatting for string minimum expression

--- string/string_minimum_expression.cpp
+++ string/string_minimum_expression.cpp
@@ -1,17 +1,21 @@
-// Given a string, s returns the index where the lexicographically minimal rotation of s begins.
-// O(n)
+// Given a string, s returns the index where the lexicographically minimal
+// rotation of s begins. O(n)
 int minimum_expression(string s) {
-    s = s+s; // if not concatenated it returns the minimum suffix index
+    s = s + s; // if not concatenated it returns the minimum suffix index
     int len = s.size(), i = 0, j = 1, k = 0;
-    while (i+k < len && j+k < len) {
-        if (s[i+k] == s[j+k]) k++;
-        else if (s[i+k] > s[j+k]) i = i+k+1, k = 0; // change to < for maximum
-        else j = j+k+1, k = 0;
-        if (i == j) j++;
+    while (i + k < len && j + k < len) {
+        if (s[i + k] == s[j + k])
+            k++;
+        else if (s[i + k] > s[j + k])
+            i = i + k + 1, k = 0; // change to < for maximum
+        else
+            j = j + k + 1, k = 0;
+        if (i == j)
+            j++;
     }
     return min(i, j);
 }
 // Usage:
 // int idx = minimum_expression(s);
 // s += s;
-// cout << s.substr(idx, n); 
+// cout << s.substr(idx, n);
