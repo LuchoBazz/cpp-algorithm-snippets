0fd3567a4caca5575974efb48590f8bafe472a99
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-11 10:33:19 -0500

    chore(formatting): apply code formatting for string prefix function

--- string/string_prefix_function.cpp
+++ string/string_prefix_function.cpp
@@ -1,14 +1,12 @@
-
-template <typename T>
-vector<int> prefix_function(const T &s) {
-    int n = (int) s.size();
+template <typename T> vector<int> prefix_function(const T &s) {
+    int n = (int)s.size();
     vector<int> lps(n, 0);
     lps[0] = 0;
     int matched = 0;
-    for(int pos = 1; pos < n; ++pos){
-        while(matched > 0 && s[pos] != s[matched])
-            matched = lps[matched-1];
-        if(s[pos] == s[matched])
+    for (int pos = 1; pos < n; ++pos) {
+        while (matched > 0 && s[pos] != s[matched])
+            matched = lps[matched - 1];
+        if (s[pos] == s[matched])
             matched++;
         lps[pos] = matched;
     }
