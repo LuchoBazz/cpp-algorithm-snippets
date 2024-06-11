e08b683903ffffca22364d285ec49ff9b645bff4
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-11 10:09:25 -0500

    chore(formatter): Update string aho corasick counter

--- string/string_aho_corasick_counter.cpp
+++ string/string_aho_corasick_counter.cpp
@@ -6,7 +6,7 @@
  */
 
 const int ALPHA = ...; // alphabet size
-const char L = ...; // first letter of the alphabet
+const char L = ...;    // first letter of the alphabet
 
 struct TrieNode {
     int next[ALPHA];
@@ -16,7 +16,7 @@ struct TrieNode {
     int cnt;
     int add;
     vector<int> idxs;
-    
+
     TrieNode() {
         fill(next, next + ALPHA, 0);
         fail_out = fail = 0;
@@ -24,14 +24,11 @@ struct TrieNode {
         end = false;
         cnt = add = 0;
     }
-    int& operator[](int idx) {
-        return next[idx];
-    }
+    int &operator[](int idx) { return next[idx]; }
 };
 
 class AhoCorasick {
-public:
-    
+  public:
     int nodes;
     int n;
     vector<TrieNode> trie;
@@ -39,20 +36,20 @@ public:
 
     AhoCorasick(const vector<string> &words) : nodes(0), words(words) {
         trie.emplace_back();
-        n = (int) words.size();
+        n = (int)words.size();
         int idx = 0;
-        for(const string &word: words)
+        for (const string &word : words)
             insert(word, idx++);
         build_ac();
     }
 
-private:
+  private:
     // insert into trie
     void insert(const string &word, int idx) {
         int state = 0;
-        for(const char &ch :word) {
+        for (const char &ch : word) {
             int c = ch - L;
-            if(!trie[state][c]) {
+            if (!trie[state][c]) {
                 trie.emplace_back();
                 trie[state][c] = ++nodes;
             }
@@ -68,32 +65,33 @@ private:
     void build_ac() {
         queue<int> q;
         // base cases
-        for(int ch = 0; ch < ALPHA; ++ch) {
-            if(trie[0][ch]) {
+        for (int ch = 0; ch < ALPHA; ++ch) {
+            if (trie[0][ch]) {
                 trie[trie[0][ch]].fail = 0;
                 trie[trie[0][ch]].fail_out = 0;
                 q.push(trie[0][ch]);
             }
         }
-        while(!q.empty()) {
-            int node = q.front(); q.pop();
-            for(int ch = 0; ch < ALPHA; ++ch) {
-                if(trie[node][ch]) {
+        while (!q.empty()) {
+            int node = q.front();
+            q.pop();
+            for (int ch = 0; ch < ALPHA; ++ch) {
+                if (trie[node][ch]) {
                     int failure = trie[node].fail;
-                    while(failure > 0 && !trie[failure][ch])
+                    while (failure > 0 && !trie[failure][ch])
                         failure = trie[failure].fail;
-                    
+
                     // create failure
                     failure = trie[failure][ch];
-                    trie[ trie[node][ch] ].fail = failure;
+                    trie[trie[node][ch]].fail = failure;
                     // create failure out
-                    if(trie[failure].end) {
-                        trie[ trie[node][ch] ].fail_out = failure;
+                    if (trie[failure].end) {
+                        trie[trie[node][ch]].fail_out = failure;
                     } else {
-                        trie[ trie[node][ch] ].fail_out = trie[failure].fail_out;
+                        trie[trie[node][ch]].fail_out = trie[failure].fail_out;
                     }
                     // propagate information
-                    trie[ trie[node][ch] ].cnt += trie[failure].cnt;
+                    trie[trie[node][ch]].cnt += trie[failure].cnt;
 
                     // add to queue
                     q.push(trie[node][ch]);
@@ -101,32 +99,33 @@ private:
             }
         }
     }
-public:
+
+  public:
     // search in automaton aho corasick
     vector<int> search(const string &text) {
         vector<int> counts(n, 0);
         int state = 0;
-        for(int i = 0; i < (int) text.size(); ++i) {
+        for (int i = 0; i < (int)text.size(); ++i) {
             int c = text[i] - L;
-            while(state > 0 && !trie[state][c])
+            while (state > 0 && !trie[state][c])
                 state = trie[state].fail;
             state = trie[state][c];
-            
+
             // traverse fail out
             int aux = state;
-            while(aux) {
-                if(trie[aux].end) {
+            while (aux) {
+                if (trie[aux].end) {
                     // process something
                     trie[aux].add++;
                 }
                 aux = trie[aux].fail_out;
             }
         }
-        
+
         // propagate counter
-        for(int i = 0; i < (int) trie.size(); ++i){
-            for(const int &idx: trie[i].idxs) {
-                counts[idx-1] += trie[i].add;
+        for (int i = 0; i < (int)trie.size(); ++i) {
+            for (const int &idx : trie[i].idxs) {
+                counts[idx - 1] += trie[i].add;
             }
         }
         return counts;
