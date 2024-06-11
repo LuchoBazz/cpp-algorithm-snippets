09da6b50b6f6b28874e1707385cb5ed04b78a442
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-11 10:21:42 -0500

    chore(formatting): apply code formatting for string kmp

--- string/string_kmp_std.cpp
+++ string/string_kmp_std.cpp
@@ -1,20 +1,18 @@
 // string_prefix_function
 
-template <typename T>
-vector<int> kmp(const T &text, const T &pattern) {
-    int n = (int) text.size();
-    int m = (int) pattern.size();
-    vector<int> lcp = prefix_function(pattern);
-    vector<int> occurrences;
+template <typename T> vector<int> kmp(const T &text, const T &pattern) {
+    int n = (int)text.size();
+    int m = (int)pattern.size();
+    vector<int> lcp = prefix_function(pattern), occurrences;
     int matched = 0;
-    for(int idx = 0; idx < n; ++idx){
-        while(matched > 0 && text[idx] != pattern[matched])
-            matched = lcp[matched-1];
-        if(text[idx] == pattern[matched])
+    for (int idx = 0; idx < n; ++idx) {
+        while (matched > 0 && text[idx] != pattern[matched])
+            matched = lcp[matched - 1];
+        if (text[idx] == pattern[matched])
             matched++;
-        if(matched == m) {
-            occurrences.push_back(idx-matched+1);
-            matched = lcp[matched-1];
+        if (matched == m) {
+            occurrences.push_back(idx - matched + 1);
+            matched = lcp[matched - 1];
         }
     }
     return occurrences;
@@ -24,10 +22,10 @@ template <typename T>
 vector<int> search_pattern(const T &text, const T &pattern) {
     return kmp(text, pattern);
 }
-//KMP - Knuth-Morris-Pratt algorithm 
-// Time Complexity: O(N), Space Complexity: O(N)
-// N: Length of text
-// Usage:
-//   string txt = "ABABABAB"; 
-//   string pat = "ABA";  
-//   vector<int> ans = search_pattern(txt, pat); {0, 2, 4}
+// KMP - Knuth-Morris-Pratt algorithm
+//  Time Complexity: O(N), Space Complexity: O(N)
+//  N: Length of text
+//  Usage:
+//    string txt = "ABABABAB";
+//    string pat = "ABA";
+//    vector<int> ans = search_pattern(txt, pat); {0, 2, 4}
