3c0908dc193b512880f4ff43a50edae7e3d83f0e
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-13 16:17:00 -0500

    chore(formatting): apply code formatting for z algorithm std

--- string/string_z_algorithm_std.cpp
+++ string/string_z_algorithm_std.cpp
@@ -1,23 +1,24 @@
-// z_array=length of the longest substring starting from s[i] which is also a prefix of s
+// z_array=length of the longest substring starting from s[i] which is also a
+// prefix of s
 vector<int> z_algorithm(const string &s) {
-    int n = (int) s.size();
+    int n = (int)s.size();
     vector<int> z_array(n);
-    int left=0, right=0;
+    int left = 0, right = 0;
     z_array[0] = 0;
-    for(int idx = 1; idx < n; ++idx) {
-        if(idx > right) {
+    for (int idx = 1; idx < n; ++idx) {
+        if (idx > right) {
             left = right = idx;
-            while(right < n && s[right-left] == s[right])
+            while (right < n && s[right - left] == s[right])
                 right++;
             z_array[idx] = right - left;
             right--;
         } else {
-            int k = idx-left;
-            if(z_array[k] < right-idx+1) {
+            int k = idx - left;
+            if (z_array[k] < right - idx + 1) {
                 z_array[idx] = z_array[k];
             } else {
                 left = idx;
-                while(right < n && s[right-left] == s[right])
+                while (right < n && s[right - left] == s[right])
                     right++;
                 z_array[idx] = right - left;
                 right--;
