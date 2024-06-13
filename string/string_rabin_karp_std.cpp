c860fae3b5956432767bfbdd21204610e6857406
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-13 16:12:45 -0500

    chore(formatting): apply code formatting for rabin karp

--- string/string_rabin_karp_std.cpp
+++ string/string_rabin_karp_std.cpp
@@ -1,16 +1,13 @@
 // string_hashing_static_compress
 
-template <typename T>
-vector<int> rabin_karp(const T &text, const T &pattern) {
-    int n = (int) text.size();
-    int m = (int) pattern.size();
-    hashing<string> txt(text);
-    hashing<string> pat(pattern);
+template <typename T> vector<int> rabin_karp(const T &text, const T &pattern) {
+    int n = (int)text.size(), m = (int)pattern.size();
+    hashing<string> txt(text), pat(pattern);
     vector<int> occurrences;
-    auto hash_pat = pat.query(0, m-1);
-    for(int i = 0; i < n-m+1; ++i){
-        auto hash_txt = txt.query(i, i+m-1);
-        if(hash_pat == hash_txt)
+    auto hash_pat = pat.query(0, m - 1);
+    for (int i = 0; i < n - m + 1; ++i) {
+        auto hash_txt = txt.query(i, i + m - 1);
+        if (hash_pat == hash_txt)
             occurrences.push_back(i);
     }
     return occurrences;
@@ -24,6 +21,6 @@ vector<int> search_pattern(const T &text, const T &pattern) {
 // Time Complexity: O(N), Space Complexity: O(N)
 // N: Length of text
 // Usage:
-//   string txt = "ABABABAB"; 
-//   string pat = "ABA";  
+//   string txt = "ABABABAB";
+//   string pat = "ABA";
 //   vector<int> ans = search_pattern(txt, pat); {0, 2, 4}
