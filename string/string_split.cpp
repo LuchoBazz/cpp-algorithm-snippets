0ca22b34e27a9c55e24f00fae6be9926698ed9eb
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-13 16:14:07 -0500

    chore(formatting): apply code formatting for string split

--- string/string_split.cpp
+++ string/string_split.cpp
@@ -1,8 +1,7 @@
 vector<string> split(string str, string separator) {
     vector<string> tokens;
-    for ( auto tok = strtok(&str[0], separator.data());
-            tok != NULL;
-            tok = strtok(NULL, separator.data())) {
+    for (auto tok = strtok(&str[0], separator.data()); tok != NULL;
+         tok = strtok(NULL, separator.data())) {
         tokens.push_back(tok);
     }
     return tokens;
