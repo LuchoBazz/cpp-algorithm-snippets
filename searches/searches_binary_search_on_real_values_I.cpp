d11e5d6d819b936555394a831069fc9141e9f406
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-14 10:21:32 -0500

    chore(formatting): apply code formatting for serches binary search on real values i

--- searches/searches_binary_search_on_real_values_I.cpp
+++ searches/searches_binary_search_on_real_values_I.cpp
@@ -1,18 +1,13 @@
 double eps = 1e-9;
-
-double n = inf;
-double low = 0.0, high = n, mid;
+double low = 0.0, high = inf, mid;
 
 while ((high - low) > eps) {
     mid = double(high + low) / 2.0;
-    if(!ok(mid)) {
+    if (!works(mid)) {
         low = mid;
     } else {
         high = mid;
     }
 }
-if(ok(low)) {
-    
-} else {
-    
-}
+
+// low or high
