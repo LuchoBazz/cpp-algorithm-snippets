974b60af9feca99120cab2a81c71429a64f17d58
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-14 10:23:18 -0500

    chore(formatting): apply code formatting for searches binary search on real values ii

--- searches/searches_binary_search_on_real_values_II.cpp
+++ searches/searches_binary_search_on_real_values_II.cpp
@@ -3,7 +3,7 @@ double low = 0.0, high = inf, mid;
 int iter = 0;
 while (iter < 300) {
     mid = double(high + low) / 2.0;
-    if (!ok(mid)) {
+    if (!works(mid)) {
         low = mid;
     } else {
         high = mid;
