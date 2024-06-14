4926e504974c9839d4028566031b5d59a063ec21
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-14 10:31:19 -0500

    chore(formatting): apply code formatting for searches ternary search ii

--- searches/searches_ternary_search_II.cpp
+++ searches/searches_ternary_search_II.cpp
@@ -1,13 +1,12 @@
 // This version is slower than the iterations version.
-double ternary_search(const function<double(double)> &func, double low, double high) {
+double ternary_search(const function<double(double)> &func, double low,
+                      double high) {
     double eps = 1e-9;
     while (high - low > eps) {
         double diff = (high - low) / 3.0;
-        double mid1 = low  + diff;
-        double mid2 = high - diff;
+        double mid1 = low + diff, mid2 = high - diff;
 
-        double f1 = func(mid1);
-        double f2 = func(mid2);
+        double f1 = func(mid1), f2 = func(mid2);
 
         if (f1 > f2) // change to < to find the maximum
             low = mid1;
