483378187868101fc95c4bda54123a3fed1cf7d1
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-14 10:38:05 -0500

    chore(formatting): apply code formatting for range query sum immutable

--- range_query/range_query_sum_immutable.cpp
+++ range_query/range_query_sum_immutable.cpp
@@ -1,23 +1,21 @@
-template<typename T>
-class PrefixSum {
-public:
-    
+template <typename T> class PrefixSum {
+  public:
     int n;
     vector<T> dp;
- 
+
     PrefixSum() : n(-1) {}
-    PrefixSum(vector<T>& nums) {
-        n = (int) nums.size();
-        if(n == 0)
+    PrefixSum(vector<T> &nums) {
+        n = (int)nums.size();
+        if (n == 0)
             return;
         dp.resize(n + 1);
         dp[0] = 0;
-        for(int i = 1; i <= n; ++i)
-            dp[i] = dp[i-1] + nums[i-1];
+        for (int i = 1; i <= n; ++i)
+            dp[i] = dp[i - 1] + nums[i - 1];
     }
-    
+
     T query(int left, int right) {
         assert(0 <= left && left <= right && right <= n - 1);
-        return dp[right+1] - dp[left];
+        return dp[right + 1] - dp[left];
     }
 };
