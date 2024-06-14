f7d2ecc4d5c4f5120d3142ca2d2a8a53ccbabd32
Luis Miguel Báez <es.luismiguelbaez@gmail.com>
2024-06-14 10:22:45 -0500

    searches binary search on real values ii

--- searches/searches_binary_search_on_real_values_II.cpp
+++ searches/searches_binary_search_on_real_values_II.cpp
@@ -1,18 +1,13 @@
-double n = inf;
-double low = 0.0, high = n, mid;
+double low = 0.0, high = inf, mid;
 
 int iter = 0;
-while(iter < 300) {
+while (iter < 300) {
     mid = double(high + low) / 2.0;
-    if(!ok(mid)) {
+    if (!ok(mid)) {
         low = mid;
     } else {
         high = mid;
     }
     iter++;
 }
-if(ok(low)) {
-    
-} else {
-    
-}
+// low or high
